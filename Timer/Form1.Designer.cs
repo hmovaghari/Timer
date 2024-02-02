8b6567938ac6343db608706315bb3fb6874c0ce5
H Movaghari <hmovaghari@gmail.com>
2024-02-02 16:45:38 +0330

    dotnet fream work 4.8

--- Timer/Form1.Designer.cs
+++ Timer/Form1.Designer.cs
@@ -32,16 +32,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtTime1 = new System.Windows.Forms.TextBox();
             this.txtTime2 = new System.Windows.Forms.TextBox();
-            this.txtTime3 = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.timer3 = new System.Windows.Forms.Timer(this.components);
-            this.btnStop3 = new System.Windows.Forms.Button();
-            this.btnPlay3 = new System.Windows.Forms.Button();
-            this.btnStop2 = new System.Windows.Forms.Button();
-            this.btnPlay2 = new System.Windows.Forms.Button();
             this.btnStop1 = new System.Windows.Forms.Button();
             this.btnPlay1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtTime1
@@ -51,80 +45,18 @@
             this.txtTime1.ReadOnly = true;
             this.txtTime1.Size = new System.Drawing.Size(178, 20);
             this.txtTime1.TabIndex = 6;
-            this.txtTime1.Text = "00:00:00";
             // 
             // txtTime2
             // 
             this.txtTime2.Location = new System.Drawing.Point(22, 88);
             this.txtTime2.Name = "txtTime2";
-            this.txtTime2.ReadOnly = true;
             this.txtTime2.Size = new System.Drawing.Size(178, 20);
             this.txtTime2.TabIndex = 7;
-            this.txtTime2.Text = "00:00:00";
-            // 
-            // txtTime3
-            // 
-            this.txtTime3.Location = new System.Drawing.Point(22, 153);
-            this.txtTime3.Name = "txtTime3";
-            this.txtTime3.ReadOnly = true;
-            this.txtTime3.Size = new System.Drawing.Size(178, 20);
-            this.txtTime3.TabIndex = 8;
-            this.txtTime3.Text = "00:00:00";
             // 
             // timer1
             // 
             this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // timer2
-            // 
-            this.timer2.Interval = 1;
-            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
-            // 
-            // timer3
-            // 
-            this.timer3.Interval = 1;
-            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
-            // 
-            // btnStop3
-            // 
-            this.btnStop3.BackgroundImage = global::Timer.Properties.Resources.Stop_48x48;
-            this.btnStop3.Location = new System.Drawing.Point(287, 147);
-            this.btnStop3.Name = "btnStop3";
-            this.btnStop3.Size = new System.Drawing.Size(49, 50);
-            this.btnStop3.TabIndex = 5;
-            this.btnStop3.UseVisualStyleBackColor = true;
-            this.btnStop3.Click += new System.EventHandler(this.btnStop3_Click);
-            // 
-            // btnPlay3
-            // 
-            this.btnPlay3.BackgroundImage = global::Timer.Properties.Resources.Play_48x48;
-            this.btnPlay3.Location = new System.Drawing.Point(223, 147);
-            this.btnPlay3.Name = "btnPlay3";
-            this.btnPlay3.Size = new System.Drawing.Size(49, 50);
-            this.btnPlay3.TabIndex = 4;
-            this.btnPlay3.UseVisualStyleBackColor = true;
-            this.btnPlay3.Click += new System.EventHandler(this.btnPlay3_Click);
-            // 
-            // btnStop2
-            // 
-            this.btnStop2.BackgroundImage = global::Timer.Properties.Resources.Stop_48x48;
-            this.btnStop2.Location = new System.Drawing.Point(287, 83);
-            this.btnStop2.Name = "btnStop2";
-            this.btnStop2.Size = new System.Drawing.Size(49, 50);
-            this.btnStop2.TabIndex = 3;
-            this.btnStop2.UseVisualStyleBackColor = true;
-            this.btnStop2.Click += new System.EventHandler(this.btnStop2_Click);
-            // 
-            // btnPlay2
-            // 
-            this.btnPlay2.BackgroundImage = global::Timer.Properties.Resources.Play_48x48;
-            this.btnPlay2.Location = new System.Drawing.Point(223, 83);
-            this.btnPlay2.Name = "btnPlay2";
-            this.btnPlay2.Size = new System.Drawing.Size(49, 50);
-            this.btnPlay2.TabIndex = 2;
-            this.btnPlay2.UseVisualStyleBackColor = true;
-            this.btnPlay2.Click += new System.EventHandler(this.btnPlay2_Click);
             // 
             // btnStop1
             // 
@@ -146,25 +78,30 @@
             this.btnPlay1.UseVisualStyleBackColor = true;
             this.btnPlay1.Click += new System.EventHandler(this.btnPlay1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(223, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Created by hmovaghari";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(355, 216);
-            this.Controls.Add(this.txtTime3);
+            this.ClientSize = new System.Drawing.Size(355, 141);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtTime2);
             this.Controls.Add(this.txtTime1);
-            this.Controls.Add(this.btnStop3);
-            this.Controls.Add(this.btnPlay3);
-            this.Controls.Add(this.btnStop2);
-            this.Controls.Add(this.btnPlay2);
             this.Controls.Add(this.btnStop1);
             this.Controls.Add(this.btnPlay1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(371, 255);
-            this.MinimumSize = new System.Drawing.Size(371, 255);
+            this.MaximumSize = new System.Drawing.Size(371, 180);
+            this.MinimumSize = new System.Drawing.Size(371, 180);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Timer";
@@ -178,16 +115,10 @@
 
         private System.Windows.Forms.Button btnPlay1;
         private System.Windows.Forms.Button btnStop1;
-        private System.Windows.Forms.Button btnStop2;
-        private System.Windows.Forms.Button btnPlay2;
-        private System.Windows.Forms.Button btnStop3;
-        private System.Windows.Forms.Button btnPlay3;
         private System.Windows.Forms.TextBox txtTime1;
         private System.Windows.Forms.TextBox txtTime2;
-        private System.Windows.Forms.TextBox txtTime3;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Timer timer2;
-        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -20,9 +20,9 @@ namespace Timer
     {
 
         // Font
-        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        [DllImport("gdi32.dll")]
         private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont,
-            IntPtr pdv, [System.Runtime.InteropServices.In] ref uint pcFonts);
+            IntPtr pdv, [In] ref uint pcFonts);
 
         public Form1()
         {
@@ -31,33 +31,30 @@ namespace Timer
 
         // Stopwatch
         Stopwatch stopWatch1 = new Stopwatch();
-        Stopwatch stopWatch2 = new Stopwatch();
-        Stopwatch stopWatch3 = new Stopwatch();
 
         // Play or Pause
         private bool play1 = false;
-        private bool play2 = false;
-        private bool play3 = false;
 
-        
-
+        // Default String
+        private string defaultString = "00:00:00";
 
         // Load Form
         private void Form1_Load(object sender, EventArgs e)
         {
             // Font
             byte[] fontData = Properties.Resources.DSEG7Modern_Bold;
-            IntPtr fontPtr = System.Runtime.InteropServices.Marshal.AllocCoTaskMem(fontData.Length);
-            System.Runtime.InteropServices.Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            IntPtr fontPtr = Marshal.AllocCoTaskMem(fontData.Length);
+            Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
             uint dummy = 0;
             PrivateFontCollection fonts = new PrivateFontCollection();
             fonts.AddMemoryFont(fontPtr, fontData.Length);
             AddFontMemResourceEx(fontPtr, (uint)fontData.Length, IntPtr.Zero, ref dummy);
-            System.Runtime.InteropServices.Marshal.FreeCoTaskMem(fontPtr);
+            Marshal.FreeCoTaskMem(fontPtr);
             Font digiFont = new Font(fonts.Families[0], 24.0F);
             txtTime1.Font = digiFont;
             txtTime2.Font = digiFont;
-            txtTime3.Font = digiFont;
+            txtTime1.Text = defaultString;
+            txtTime2.Text = defaultString;
         }
 
         // Stopwatch 1 - Play or Pause
@@ -83,86 +80,24 @@ namespace Timer
         private void timer1_Tick(object sender, EventArgs e)
         {
             txtTime1.Text = string.Format("{0:hh\\:mm\\:ss}", stopWatch1.Elapsed);
+            if (txtTime2.Text != defaultString && txtTime1.Text == txtTime2.Text)
+            {
+                MessageBox.Show("Timer Is Stop", Text, MessageBoxButtons.OK,
+                                MessageBoxIcon.Information, MessageBoxDefaultButton.Button1,
+                                MessageBoxOptions.ServiceNotification);
+                btnStop1_Click(sender, e);
+            }
         }
 
         // Stopwatch 1 - Stop
         private void btnStop1_Click(object sender, EventArgs e)
         {
+            txtTime1.Text = defaultString;
             play1 = false;
             btnPlay1.BackgroundImage = Properties.Resources.Play_48x48;
             timer1.Stop();
             stopWatch1.Stop();
             stopWatch1.Reset();
-        }
-
-        // Stopwatch 2 - Play or Pause
-        private void btnPlay2_Click(object sender, EventArgs e)
-        {
-            if (play2)
-            {
-                play2 = false;
-                btnPlay2.BackgroundImage = Properties.Resources.Play_48x48;
-                timer2.Stop();
-                stopWatch2.Stop();
-            }
-            else
-            {
-                play2 = true;
-                btnPlay2.BackgroundImage = Properties.Resources.Pause_48x48;
-                timer2.Start();
-                stopWatch2.Start();
-            }
-        }
-
-        // Stopwatch 2 - Write time in textbox
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            txtTime2.Text = string.Format("{0:hh\\:mm\\:ss}", stopWatch2.Elapsed);
-        }
-
-        // Stopwatch 2 - Stop
-        private void btnStop2_Click(object sender, EventArgs e)
-        {
-            play2 = false;
-            btnPlay2.BackgroundImage = Properties.Resources.Play_48x48;
-            timer2.Stop();
-            stopWatch2.Stop();
-            stopWatch2.Reset();
-        }
-
-        // Stopwatch 3 - Play or Pause
-        private void btnPlay3_Click(object sender, EventArgs e)
-        {
-            if (play3)
-            {
-                play3 = false;
-                btnPlay3.BackgroundImage = Properties.Resources.Play_48x48;
-                timer3.Stop();
-                stopWatch3.Stop();
-            }
-            else
-            {
-                play3 = true;
-                btnPlay3.BackgroundImage = Properties.Resources.Pause_48x48;
-                timer3.Start();
-                stopWatch3.Start();
-            }
-        }
-
-        // Stopwatch 3 - Write time in textbox
-        private void timer3_Tick(object sender, EventArgs e)
-        {
-            txtTime3.Text = string.Format("{0:hh\\:mm\\:ss}", stopWatch3.Elapsed);
-        }
-
-        // Stopwatch 3 - Stop
-        private void btnStop3_Click(object sender, EventArgs e)
-        {
-            play3 = false;
-            btnPlay3.BackgroundImage = Properties.Resources.Play_48x48;
-            timer3.Stop();
-            stopWatch3.Stop();
-            stopWatch3.Reset();
         }
     }
 }
